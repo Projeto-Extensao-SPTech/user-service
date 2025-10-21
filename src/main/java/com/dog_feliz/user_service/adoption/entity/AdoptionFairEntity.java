19f0ce71f34b147f7eacbb3b4853e4d2e17986d4
lucasaiello-dev <lucas.aiello@sptech.school>
2025-10-21 19:05:41 -0300

    test

--- src/main/java/com/dog_feliz/user_service/adoption/entity/AdoptionFairEntity.java
+++ src/main/java/com/dog_feliz/user_service/adoption/entity/AdoptionFairEntity.java
@@ -13,6 +13,5 @@ public class AdoptionFairEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private ZonedDateTime date;
-    @Times
     private ZonedDateTime createdAt;
 }
