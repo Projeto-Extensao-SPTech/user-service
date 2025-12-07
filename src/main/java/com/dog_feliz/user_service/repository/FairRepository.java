068e652b090287328be89384c3410c5df4d31890
GiovanaZukauskas <giovana.ratao@sptech.school>
2025-12-07 18:00:41 -0300

    Subindo dashboard

--- src/main/java/com/dog_feliz/user_service/repository/FairRepository.java
+++ src/main/java/com/dog_feliz/user_service/repository/FairRepository.java
@@ -1,12 +1,35 @@
 package com.dog_feliz.user_service.repository;
 
+import com.dog_feliz.user_service.controller.dto.DashboardFairKpiDto;
 import com.dog_feliz.user_service.entity.FairEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface FairRepository extends JpaRepository<FairEntity, Long> {
+
     List<FairEntity> findByFairDateGreaterThan(LocalDate date);
+
+
+    @Query("""
+        SELECT FUNCTION('TO_CHAR', f.fairDate, 'YYYY-MM') AS month, SUM(f.interest) AS total
+        FROM FairEntity f
+        GROUP BY FUNCTION('TO_CHAR', f.fairDate, 'YYYY-MM')
+        ORDER BY total DESC
+    """)
+    List<Object[]> findMonthWithMostInterestRaw();
+
+
+    @Query("""
+        SELECT f.address.street AS label, SUM(f.interest) AS total
+        FROM FairEntity f
+        GROUP BY f.address.street
+        ORDER BY total DESC
+    """)
+    List<Object[]> findLocationWithMostInterestRaw();
 }
+
