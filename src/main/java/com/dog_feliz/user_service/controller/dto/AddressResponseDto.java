dad8ed407a3059e8d1bf0a8fe6cf0df50e2abc24
Victor-Hugo1 <victor.hsouza@sptech.school>
2025-12-07 16:36:18 -0300

    adjust address

--- src/main/java/com/dog_feliz/user_service/controller/dto/AddressResponseDto.java
+++ src/main/java/com/dog_feliz/user_service/controller/dto/AddressResponseDto.java
@@ -9,13 +9,17 @@ public class AddressResponseDto {
     private String zipCode;
     private Integer number;
     private String street;
+    private String city;
+    private String state;
     private ZonedDateTime createdAt;
 
-    public AddressResponseDto(Long id, String zipCode, Integer number, String street, ZonedDateTime createdAt) {
+    public AddressResponseDto(Long id, String zipCode, Integer number, String street, String city, String state, ZonedDateTime createdAt) {
         this.id = id;
         this.zipCode = zipCode;
         this.number = number;
         this.street = street;
+        this.city = city;
+        this.state = state;
         this.createdAt = createdAt;
     }
 
@@ -24,6 +28,8 @@ public class AddressResponseDto {
         this.zipCode = address.getZipCode();
         this.number = address.getNumber();
         this.street = address.getStreet();
+        this.city = address.getCity();
+        this.state = address.getState();
         this.createdAt = address.getCreatedAt();
     }
 
@@ -41,6 +47,14 @@ public class AddressResponseDto {
 
     public String getStreet() {
         return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
     }
 
     public ZonedDateTime getCreatedAt() {
