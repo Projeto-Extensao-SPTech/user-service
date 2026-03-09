725ababc254c9a9e46463923f5952d67376c6fb9
lucasaiello-dev <lucas.aiello@sptech.school>
2026-03-09 01:03:32 -0300

    feat: add service layer and adjusts entity and repository to attend business methods

--- src/main/java/com/dog_feliz/user_service/repository/RefreshTokenRepository.java
+++ src/main/java/com/dog_feliz/user_service/repository/RefreshTokenRepository.java
@@ -2,10 +2,18 @@ package com.dog_feliz.user_service.repository;
 
 import com.dog_feliz.user_service.entity.RefreshTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
-    Optional<RefreshTokenEntity> findByTokenHashAndRevokedFalse(String tokenHash);
-    List<RefreshTokenEntity> findAllByUserIdAndRevokedFalse(Long userId);
+    Optional<RefreshTokenEntity> findByTokenHash(String tokenHash);
+    @Modifying
+    @Query("""
+       UPDATE RefreshTokenEntity t
+       SET t.revoked = true
+       WHERE t.root = :root
+       """)
+    void revokeAllByRoot(@Param("root") RefreshTokenEntity root);
 }
---
+++ src/main/java/com/dog_feliz/user_service/service/RefreshTokenService.java
@@ -0,0 +1,90 @@
+package com.dog_feliz.user_service.service;
+
+import com.dog_feliz.user_service.entity.RefreshTokenEntity;
+import com.dog_feliz.user_service.repository.RefreshTokenRepository;
+import com.dog_feliz.user_service.shared.crypto.hash.StringHasher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Service
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final StringHasher stringHasher;
+    private final Integer REFRESH_TOKEN_EXPIRATION_TIME;
+
+    public RefreshTokenService(
+            RefreshTokenRepository refreshTokenRepository,
+            StringHasher stringHasher,
+            @Value("${security.refresh-token.expiration.time}") Integer REFRESH_TOKEN_EXPIRATION_TIME
+    ) {
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.stringHasher = stringHasher;
+        this.REFRESH_TOKEN_EXPIRATION_TIME = REFRESH_TOKEN_EXPIRATION_TIME;
+    }
+
+    public String generate(String oldRefreshToken, Long userId) {
+        String refreshToken;
+        if (oldRefreshToken == null) {
+            return create(
+                    userId,
+                    null,
+                    null
+            );
+        }
+        return refresh(oldRefreshToken);
+    }
+
+    private String refresh(String actualRefreshToken) {
+        RefreshTokenEntity token = findRefreshTokenByTokenHash(actualRefreshToken);
+        if (token.isRevoked()) {
+            revokeFamily(token.getRoot());
+            throw new IllegalStateException("Refresh token reuse detected");
+        }
+
+        if (token.isExpired()) {
+            throw new IllegalStateException("Refresh token expired");
+        }
+
+        token.revoke();
+        refreshTokenRepository.save(token);
+
+        return create(
+                token.getUserId(),
+                token,
+                token.getRoot()
+        );
+    }
+
+    private String create(Long userId, RefreshTokenEntity parent, RefreshTokenEntity root) {
+        String refreshToken = RefreshTokenEntity.generateRefreshToken();
+        RefreshTokenEntity entity = new RefreshTokenEntity(
+                null,
+                userId,
+                stringHasher.hash(refreshToken),
+                LocalDateTime.now().plus(REFRESH_TOKEN_EXPIRATION_TIME, ChronoUnit.MILLIS),
+                false,
+                parent,
+                root
+        );
+        RefreshTokenEntity saved = refreshTokenRepository.save(entity);
+
+        if (root == null) {
+            saved.setRoot(saved);
+        }
+        return refreshToken;
+    }
+
+    private void revokeFamily(RefreshTokenEntity root) {
+        refreshTokenRepository.revokeAllByRoot(root);
+    }
+
+    private RefreshTokenEntity findRefreshTokenByTokenHash(String refreshToken) {
+        return refreshTokenRepository
+                .findByTokenHash(stringHasher.hash(refreshToken))
+                .orElseThrow(() -> new IllegalArgumentException("Invalid refresh token to revoke"));
+
+    }
+}
