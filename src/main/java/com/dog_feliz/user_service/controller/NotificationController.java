18f541e357082c36b7aba3f5e7ef1f582b21ba70
Victor-Hugo1 <victor.hsouza@sptech.school>
2026-04-03 15:48:38 -0300

    merge develop and adjusts

--- src/main/java/com/dog_feliz/user_service/controller/NotificationController.java
+++ src/main/java/com/dog_feliz/user_service/controller/NotificationController.java
@@ -2,7 +2,6 @@ package com.dog_feliz.user_service.controller;
 
 import com.dog_feliz.user_service.controller.dto.NotificationRequestDto;
 import com.dog_feliz.user_service.service.NotificationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import com.dog_feliz.user_service.service.ValidationService;
 import org.springframework.http.ResponseEntity;
@@ -23,13 +22,9 @@ public class NotificationController {
 
     @PostMapping
     private ResponseEntity<Void> register(@RequestBody NotificationRequestDto notificationRequest) {
+        validationService.verifyIsAdminUser();
         notificationService.register(notificationRequest);
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
-    private ResponseEntity<NotificationResponseDto> register(@RequestBody NotificationRequestDto notificationRequest) {
-        validationService.verifyIsAdminUser();
-        return ResponseEntity
-                .status(202)
-                .body(toResponse(notificationService.register(notificationRequest)));
     }
 
     @RequestMapping(method = RequestMethod.HEAD, path = "/send-today-notifications")
